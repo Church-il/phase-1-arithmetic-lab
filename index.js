336f460332722364dc468e7bf8ec6765fb81f2f6
Michael Akoko <akokomichael37@gmail.com>
2024-06-27 18:52:43 +0300

    complete my lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.ceil(Math.random()) + 2;
+console.log(random);
+
+let num3 = 16;
+let num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+let num5 = 18;
+let num6 = 15;
+let num7 = 20;
+let num8 = 11;
+const max = Math.max(num5, num6, num7, num8);
+console.log(max);
